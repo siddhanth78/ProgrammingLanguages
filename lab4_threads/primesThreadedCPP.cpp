d408a66cbf80203545f7ff31841be37a9ccd8fde
zvoverman <zvoverman@gmail.com>
2022-10-19 14:56:47 -0600

    fixed threading errors - works!

--- lab4_threads/primesThreadedCPP.cpp
+++ lab4_threads/primesThreadedCPP.cpp
@@ -3,38 +3,51 @@
 
 // compile like: g++ vecAdd.cc -std=c++11 -pthread -o vecAdd
 int isPrime(int num);
-void myRun(int start, int stop, int* count);
+void myRun(int start, int stop, int index, int* count);
 
 int main() {
-    int count = 0;
     int low = 10;
     int high = 100;
     int range = high - low;
 
     int numThreads = 4;
     std::thread* ths[numThreads];
+
+    int* count = (int*)malloc(numThreads * sizeof(int));
+
     for (int i = 0; i < numThreads; i++) {
-        int start = low + (range / numThreads) * i; // what out for int division
+        int start = low + (range / numThreads) * i; 
         int stop = low + (range / numThreads) * (i + 1);
+
+        int index = i;
+
         if(i + 1 == 4)
             stop = high;
 
-        std::thread* th = new std::thread(myRun, start, stop, &count);
+        std::thread* th = new std::thread(myRun, start, stop, index, count);
         ths[i] = th;
     }
 
     for (int i = 0; i < numThreads; i++) {
         ths[i]->join();
     }
+
+    int total = 0;
+    for(int i = 0; i < numThreads; i++) {
+        total += count[i];
+    }
+
+    std::cout << "total: " << total << "\n";
+    free(count);
 }
 
-void myRun(int start, int stop, int* count) {
-    std::cout << "start: " << start << ", stop: " << stop << "\n";
+void myRun(int start, int stop, int index, int* count) {
+    //std::cout << "start: " << start << ", stop: " << stop << "\n";
 
     for (int i = start; i < stop; i++) {
-        *count += isPrime(i);
+        count[index] += isPrime(i);
     }
-    std::cout << "count: " << *count << "\n";
+    //std::cout << "count: " << *count << "\n";
 }
 
 int isPrime(int num) {
